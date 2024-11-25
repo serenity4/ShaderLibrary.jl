e7cd2dd8ac3336294664620f0082f5d67bd1b112
serenity4 <cedric.bel@hotmail.fr>
2024-11-25 16:17:50 +0100

    Support alpha component for gradient

--- src/library/gradient.jl
+++ src/library/gradient.jl
@@ -1,18 +1,20 @@
-struct Gradient <: Material end
+struct Gradient{T} <: Material end
 
-function gradient_vert(position, color, index, (; data)::PhysicalRef{InvocationData})
+Gradient() = Gradient{Vec3}()
+
+function gradient_vert(position, color, index, (; data)::PhysicalRef{InvocationData}, ::Type{T}) where {T}
   @swizzle position.xyz = world_to_screen_coordinates(data.vertex_locations[index + 1U], data)
-  @swizzle color.rgb = @load data.vertex_data[index + 1U]::Vec3
+  color[] = vec4(@load data.vertex_data[index + 1U]::T)
 end
 
 function gradient_frag(color_output, color_input)
   color_output[] = color_input
 end
 
-function Program(::Type{Gradient}, device)
-  vert = @vertex device gradient_vert(::Mutable{Vec4}::Output{Position}, ::Mutable{Vec4}::Output, ::UInt32::Input{VertexIndex}, ::PhysicalRef{InvocationData}::PushConstant)
+function Program(::Type{Gradient{T}}, device) where {T}
+  vert = @vertex device gradient_vert(::Mutable{Vec4}::Output{Position}, ::Mutable{Vec4}::Output, ::UInt32::Input{VertexIndex}, ::PhysicalRef{InvocationData}::PushConstant, ::Type{T})
   frag = @fragment device gradient_frag(::Mutable{Vec4}::Output, ::Vec4::Input)
   Program(vert, frag)
 end
 
-interface(::Gradient) = Tuple{Vector{Vec3},Nothing,Nothing}
+interface(::Gradient{T}) where {T} = Tuple{Vector{T},Nothing,Nothing}
